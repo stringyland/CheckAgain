94a9ab212072b618a8cc07e3495d86d43e48737d
Julie Grundy <stringy.j@gmail.com>
2015-04-17 17:31:55 +0800

    Switched list styles to Bootstrap
    class equals list list-item item

--- js/ca.js
+++ js/ca.js
@@ -11,39 +11,34 @@ var editTextA = "Edit";
 var editTextB = "Done";
 
 $(document).ready(function() {
-// add anything that's js-dependant
-
-// remove anything that's replaced by js-dependant stuff
-
-
-
-$(editMaster).click(function(event) {
-	$(masterButtons).toggleClass('hidden');
-	$(editMaster).toggleClass('js-is-editing');
-	if ($(editMaster).hasClass('js-is-editing')) {
-		$(editMaster).text("Finish editing");
-	} else {
-		$(editMaster).text("Edit Master list");
-	}
-});
-$(editList).click(function(event) {
-	$(listButtons).toggleClass('hidden');
-	$(editList).toggleClass('js-is-editing');
-	if ($(editList).hasClass('js-is-editing')) {
-		$(editList).text("Finish editing");
-	} else {
-		$(editList).text("Edit this list");
-	}
-});
-
-		// if delete button
-			// delete function: this list/item
-		// if move button
-			// move function: this list/item
-		// if add button
-			// add function: context list or item
-		
-	// if back button
+	// add anything that's js-dependant
+	
+	// remove anything that's replaced by js-dependant stuff
+	
+	
+	// EDIT LINKS
+	
+	$(editMaster).click(function(event) {
+		$(masterButtons).toggleClass('hidden');
+		$(editMaster).toggleClass('js-is-editing');
+		if ($(editMaster).hasClass('js-is-editing')) {
+			$(editMaster).text("Finish editing");
+		} else {
+			$(editMaster).text("Edit Master list");
+		}
+	});
+	$(editList).click(function(event) {
+		$(listButtons).toggleClass('hidden');
+		$(editList).toggleClass('js-is-editing');
+		if ($(editList).hasClass('js-is-editing')) {
+			$(editList).text("Finish editing");
+		} else {
+			$(editList).text("Edit this list");
+		}
+	});
+	
+	// BACK LINKS
+	
 	$('#ca-back-master').click(function(event) {
 		event.preventDefault();
 		masterList.removeClass('hidden');
@@ -54,17 +49,25 @@ $(editList).click(function(event) {
 		});
 		switchTools();
 	});
-	// if list title
+	
+	// LIST LINKS
 	$( "ul.m-master li a" ).click(function(event) {
 		//get which link
 		var whichID = this.id;
 		// do function
 		showList(whichID);
 	});
+	
+	
+	
+	
 
-	// if settings
-		// load function: settings
-		
+
+});
+
+// FUNCTIONS 
+// ---------------------------
+			
 function showList(whichTitle) {
 	//turn title id into list id
 	var stringArray = whichTitle.split('-'); //ca-ml-X, we want to find out X
@@ -94,11 +97,8 @@ function toggleEditText(whichLink) {
 	}
 }
 
+
 function switchTools(){
 	listTools.toggleClass('hidden');
 	masterTools.toggleClass('hidden');
 }
-
-
-});
-
